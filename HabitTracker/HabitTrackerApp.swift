9034fda987916b155f76bdf25287901288590804
Jasper Tan <jatan@Jaspers-Mac-mini.local>
2024-12-06 10:57:07 -0800

    initial commit

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Jasper Tan on 12/6/24.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
